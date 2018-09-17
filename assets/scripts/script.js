e527ddbb5ca757bd1bf0024484d3520850854b9c
JAWilmer <39922930+JAWilmer@users.noreply.github.com>
2018-09-16 21:22:40 -0500

    Add files via upload

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -11,39 +11,6 @@ $(document).ready(function () {
 	  };
 	  firebase.initializeApp(config);
 
-
-	// // Signs-in to app with pop-up
-	// function signIn() {
-	// 	var provider = new firebase.auth.GoogleAuthProvider();
-	// 	firebase.auth().signInWithPopup(provider);
-	// }
-
-	// // Signs-out of app
-	// function signOut() {
-	// 	firebase.auth().signOut();
-	// }
-
-	// // Initiate firebase auth.
-	// function initFirebaseAuth() {
-	// 	// Listen to auth state changes.
-	// 	firebase.auth().onAuthStateChanged(authStateObserver);
-	// }
-
-	// // Returns the signed-in user's profile Pic URL.
-	// function getProfilePicUrl() {
-	// 	return firebase.auth().currentUser.photoURL || '/images/profile_placeholder.png';
-	// }
-
-	// // Returns the signed-in user's display name.
-	// function getUserName() {
-	// 	return firebase.auth().currentUser.displayName;
-	// }
-
-	// // Returns true if a user is signed-in.
-	// function isUserSignedIn() {
-	// 	return !!firebase.auth().currentUser;
-	// }
-
 	var database = firebase.database();
 
 	// COLLECTS AND ADDS TRAINS
